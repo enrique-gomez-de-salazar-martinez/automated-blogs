7094c82bbc4e43e764bbdb105004ec55cbddc71e
Enrique Gomez de Salazar Martinez <tell.ur.gf420@gmail.com>
2025-03-31 15:22:49 +0200

    Update script.js

--- code/script.js
+++ code/script.js
@@ -2,6 +2,7 @@
 
 /* 
    If your girlfriend did something behind your back I think you'd deserve to know as well.
+   This isn't out of hate or revenge. I think it's the right thing and I have pride in being honest.
    I hope you learn to understand yourself better and to stop acting out of fear and insecurity.
    That's not a fun way to live and it leads to regret, and (unintentionally) hurting people.
 */
