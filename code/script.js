5b993710c5b5008807c35f5cd39d4bf913f0cd0a
Enrique Gomez de Salazar Martinez <tell.ur.gf420@gmail.com>
2025-03-31 09:51:34 +0200

    Create script.js

---
+++ code/script.js
@@ -0,0 +1,7 @@
+// The actual code is private
+
+/* I hope you learn to understand yourself better and to stop acting out of fear and insecurity.
+   That's not a fun way to live and it leads to a lot of regret.
+   Until then you'll probably continue to hurt people, even if you don't mean to.
+   Just speaking based on my own experience. I don't have it all figured out either.
+*/
