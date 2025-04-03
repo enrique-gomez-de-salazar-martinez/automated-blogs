832a194d3d4f74a9322a50322e9267a1a3362384
Enrique Gomez de Salazar Martinez <tell.ur.gf420@gmail.com>
2025-04-03 09:03:53 +0200

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,7 +1,7 @@
 // The actual code is private
 
 /* 
-   If your girlfriend did something behind your back I think you'd deserve to know as well.
+   Your girlfriend deserves to know who she's really in a relationship with, as would you if the roles were reversed.
    This isn't out of hate or revenge. I think it's the right thing and I have pride in being honest.
    I hope you learn to understand yourself better and to stop acting out of fear and insecurity.
    That's not a fun way to live and it leads to regret, and (unintentionally) hurting people.
